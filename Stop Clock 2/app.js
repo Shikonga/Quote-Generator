f100ee395f7689a84bfd00178e6bdcad1b84c77f
shikonga <wanjikungaruiya71@gmail.com>
2024-09-09 10:32:18 +0300

    Initial commit

---
+++ Stop Clock 2/app.js
@@ -0,0 +1,21 @@
+// startbtn should start counting
+// stopbtn to stop
+//resetbtn to rest to 00.00.00
+// hours , mins and secs variable
+// function for each button
+// clearinterval for resetBtn 0
+// clearInterval for stopBtn to stop
+
+
+
+
+let [hours, minutes, seconds] = [0, 0, 0];
+const stopwatchElement = document.getElementById("stopwatchTime");
+
+
+function startTimer() {
+    // starts counting seconds until 60 then jumps to minutes.
+    stopwatchElement.innerHTML = "blah blah";
+    c
+    
+}
